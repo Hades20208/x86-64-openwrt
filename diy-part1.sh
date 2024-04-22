0bca5a472adbd4ba86b0c573fdac794ed5e64649
Hades20208 <73427087+Hades20208@users.noreply.github.com>
2024-04-23 05:42:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,24 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Take the x86_64 platform as an example
+tar xjf openwrt-sdk-21.02.3-x86-64_gcc-8.4.0_musl.Linux-x86_64.tar.xz
+# Go to the SDK root dir
+cd OpenWrt-sdk-*-x86_64_*
+# First run to generate a .config file
+make menuconfig
+./scripts/feeds update -a
+./scripts/feeds install -a
+# Get Makefile
+git clone --depth 1 --branch master --single-branch --no-checkout https://github.com/muink/luci-app-netdata.git package/luci-app-netdata
+pushd package/luci-app-netdata
+umask 022
+git checkout
+popd
+# Select the package LuCI -> Applications -> luci-app-netdata
+make menuconfig
+# Start compiling
+make package/luci-app-netdata/compile V=99
